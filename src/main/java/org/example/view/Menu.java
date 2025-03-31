531a8d6e741ed59633f543261a472c50bf922ae7
alliciarocha <allicia30300@gmail.com>
2025-03-31 18:12:30 -0300

    Update Menu.java

--- src/main/java/org/example/view/Menu.java
+++ src/main/java/org/example/view/Menu.java
@@ -7,26 +7,38 @@ import java.util.Scanner;
 
 public class Menu {
     public static void show(){
-        Reflections reflections = new Reflections("org.example.model");
-
-        Set<Class<? extends IOperation>> classes = reflections.getSubTypesOf(IOperation.class);
-
-        for (Class<? extends IOperation> clazz : classes) {
-            System.out.println(clazz.getSimpleName());
-        }
-        System.out.println("Sair");
-        System.out.println("Digite a operação desejada: ");
         Scanner sc = new Scanner(System.in);
-        String operacao = sc.nextLine();
-        System.out.println("Digite o Primeiro Valor");
-        int a = sc.nextInt();
-        System.out.println("Digite o Segundo Valor");
-        int b = sc.nextInt();
 
-        ControladorCalculadora control = new ControladorCalculadora();
+        while(true){
+            Reflections reflections = new Reflections("org.example.model");
+            Set<Class<? extends IOperation>> classes = reflections.getSubTypesOf(IOperation.class);
 
-        int resultado = control.calcular(operacao, a, b);
+            System.out.println("\nOperações:");
+            for (Class<? extends IOperation> clazz : classes) {
+                System.out.println("- " + clazz.getSimpleName());
+            }
+            System.out.println("- Sair");
+            System.out.println("Digite a Operação Desejada: ");
 
-        System.out.println("Resultado: " + resultado);
+
+            String operacao = sc.nextLine();
+
+            if (operacao.equalsIgnoreCase("Sair")) {
+                System.out.println("Saindo...");
+                break;
+            }
+
+            System.out.println("Digite o Primeiro Valor");
+            int a = sc.nextInt();
+            System.out.println("Digite o Segundo Valor");
+            int b = sc.nextInt();
+
+            ControladorCalculadora control = new ControladorCalculadora();
+
+            int resultado = control.calcular(operacao, a, b);
+
+            System.out.println("Resultado: " + resultado);
+        }
+        sc.close();
     }
 }
